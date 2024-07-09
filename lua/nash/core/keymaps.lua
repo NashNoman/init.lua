99b4a7ef824c46e826bf28f4bc1cd664c410165f
NashNoman <nashwan2k@gmail.com>
2024-07-10 00:27:49 +0300

    Update keymaps

--- lua/nash/core/keymaps.lua
+++ lua/nash/core/keymaps.lua
@@ -22,8 +22,17 @@ map("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- spli
 map("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 map("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+-- buffer/tab management
+map("n", "<leader>bp", "<cmd>BufferLinePick<CR>", { desc = "Bufferline pick" })
+map("n", "<leader>bc", "<cmd>BufferLinePickClose<CR>", { desc = "Bufferline pick close" })
+map("n", "<leader>tp", "<cmd>BufferLineCyclePrev<CR>", { desc = "Go to previous buffer" })
+map("n", "<leader>tn", "<cmd>BufferLineCycleNext<CR>", { desc = "Go to next buffer" })
+map("n", "<leader>tco", "<cmd>BufferLineCloseOthers<CR>", { desc = "Close other buffers" })
+map("n", "<leader>tcl", "<cmd>BufferLineCloseLeft<CR>", { desc = "Close buffers to the left" })
+map("n", "<leader>tcr", "<cmd>BufferLineCloseRight<CR>", { desc = "Close buffers to the right" })
+
+--[[ map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab ]]
