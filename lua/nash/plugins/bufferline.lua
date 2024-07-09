68a3ad4219833a759b2c2b22cc8c9de767e67f79
NashNoman <nashwan2k@gmail.com>
2024-07-10 00:27:28 +0300

    Configure bufferline.nvim

--- lua/nash/plugins/bufferline.lua
+++ lua/nash/plugins/bufferline.lua
@@ -1,12 +1,30 @@
 return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-
   version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
+  config = function()
+    local bufferline = require("bufferline")
 
-    },
-  },
+    bufferline.setup({
+      options = {
+        mode = "buffers",
+        style_preset = bufferline.style_preset.default,
+        diagnostics = "nvim_lsp",
+        diagnostics_update_in_insert = true,
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+        end,
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            separator = true,
+          },
+        },
+        numbers = "ordinal",
+        indicator = { style = "underline" },
+      },
+    })
+  end,
 }
