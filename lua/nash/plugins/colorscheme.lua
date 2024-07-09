75a940350ddc1a7beb3679e9dd416ffb5fcb8260
NashNoman <nashwan2k@gmail.com>
2024-07-09 17:59:39 +0300

    Set nvim transparency

--- lua/nash/plugins/colorscheme.lua
+++ lua/nash/plugins/colorscheme.lua
@@ -4,6 +4,9 @@ return {
     priority = 1000,
     config = function()
       vim.cmd.colorscheme("github_dark_tritanopia")
+
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
   },
   {
